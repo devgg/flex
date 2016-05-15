66660db7af6ab780b5fabb7e3bbe2a5eb18aaa4c
devgg <flo.gauger@gmail.com>
2016-05-15 15:13:08 +0200

    Finish the container properties

--- flex.js
+++ flex.js
@@ -1,7 +1,11 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    var $selectedItem = $("#flex-item-1");
-    var $flexContainer = $("#flex-container");
+    var selectedNumber = $(".flex-item-1");
+    var $flexContainerDirection = $("#flex-container-direction");
+    var $flexContainerJustify = $("#flex-container-justify");
+    var $flexContainerAlignItems = $("#flex-container-align-items");
+    var $flexContainerAlignContent = $("#flex-container-align-content");
+    var $flexContainerWrap = $("#flex-container-wrap");
     var $flexItem = $(".flex-item");
 
     var flexContainerColor = "white";
@@ -9,77 +13,110 @@ $( document ).ready(function() {
 
     var $itemControl = $("#item-control");
 
-    $flexContainer.hover(function(event) {
-        //$flexContainer.css("background-color", flexContainerColorHover);
-    }, function() {
-        //$flexContainer.css("background-color", flexContainerColor);
-    });
+    var numberItems = 5;
 
-
-    $flexItem.hover(function(event) {
-        //$flexContainer.css("background-color", flexContainerColor);
-    }, function() {
-        //$flexContainer.css("background-color", flexContainerColorHover);
-    });
 
     function isRowDirection(direction) {
         return direction.charAt(0) === "r";
     }
 
     function changeFlexDirection(direction) {
-        if(isRowDirection($flexContainer.css("flex-direction")) !== isRowDirection(direction)) {
-            var animation1 = {};
-            var animation2 = {};
-            animation1[isRowDirection(direction) ? "width" : "height"] = 400;
-            animation2[isRowDirection(direction) ? "height" : "width"] = 100;
-            $flexContainer.animate(animation1, 400, "swing", function () {
-                $flexContainer.css("flex-direction", direction);
-                $flexContainer.animate(animation2);
-            });
+        if (isRowDirection($flexContainerDirection.css("flex-direction")) !== isRowDirection(direction)) {
+
+            if (isRowDirection(direction)) {
+                $flexContainerDirection.animate({width: 400}, 400, "swing", function () {
+                    $flexContainerDirection.css("flex-direction", direction);
+                    $flexContainerDirection.animate({height: 100});
+                });
+            } else {
+                $flexContainerDirection.animate({height: 400}, 400, "swing", function () {
+                    $flexContainerDirection.css("flex-direction", direction);
+                    $flexContainerDirection.animate({width: 100});
+                });
+            }
+
         } else {
-            $flexContainer.css("flex-direction", direction);
+            $flexContainerDirection.css("flex-direction", direction);
         }
     }
 
-    $flexItem.click(function() {
+    $flexItem.click(function () {
         $selectedItem = $(this)
         var itemNumber = $selectedItem.attr("data-number");
         $itemControl.removeClass();
         $itemControl.addClass("item-" + itemNumber);
     });
 
-    $("html").click(function() {
+    $("html").click(function () {
         $(".dropdown-content").css("display", "none");
     });
 
-    $("#container-control .dropdown-label").click(function (event) {
+    $(".dropdown").click(function (event) {
+        var currentValue = $(".dropdown ." + $(this).attr("data-propertyName")).css("display");
         $(".dropdown-content").css("display", "none");
-        $("#container-control ." + $(this).attr("data-propertyName")).css("display", "flex");
-        event.stopPropagation();
-    })
+        if (currentValue === "none") {
 
-    $("#container-control .dropdown-item").click(function() {
-        var property = $(this).attr("data-propertyName");
-        var value = $(this).attr("data-propertyValue");
-        if (property === "flex-direction") {
-            changeFlexDirection(value);
-        } else {
-            $flexContainer.css(property, value);
+            $(".dropdown ." + $(this).attr("data-propertyName")).toggle();
         }
         event.stopPropagation();
     })
 
-    $("#item-control .dropdown-label").click(function (event) {
-        $(".dropdown-content").css("display", "none");
-        $("#item-control ." + $(this).attr("data-propertyName")).css("display", "flex");
-        event.stopPropagation();
-    })
-
-    $("#item-control .dropdown-item").click(function() {
-        var property = $(this).attr("data-propertyName");
+    $("#flex-row-direction").find(".dropdown-item").click(function () {
         var value = $(this).attr("data-propertyValue");
-        $selectedItem.css(property, value);
+        $("#flex-row-direction").find(".dropdown-button").text(value + ";");
+        changeFlexDirection(value);
         event.stopPropagation();
-    })
+    });
+
+    $("#flex-row-justify").find(".dropdown-item").click(function () {
+        var value = $(this).attr("data-propertyValue");
+        $("#flex-row-justify").find(".dropdown-button").text(value + ";");
+        $flexContainerJustify.css("justify-content", value);
+        event.stopPropagation();
+    });
+
+    $("#flex-row-align-items").find(".dropdown-item").click(function () {
+        var value = $(this).attr("data-propertyValue");
+        $("#flex-row-align-items").find(".dropdown-button").text(value + ";");
+        $flexContainerAlignItems.css("align-items", value);
+        event.stopPropagation();
+    });
+
+    $("#flex-row-align-content").find(".dropdown-item").click(function () {
+        var value = $(this).attr("data-propertyValue");
+        $("#flex-row-align-content").find(".dropdown-button").text(value + ";");
+        $flexContainerAlignContent.css("align-content", value);
+        event.stopPropagation();
+    });
+
+    $("#flex-row-wrap").find(".dropdown-item").click(function () {
+        var value = $(this).attr("data-propertyValue");
+        var minHeight;
+        if (value === "nowrap") {
+            $flexContainerWrap.css("align-items", "center");
+            minHeight = "50px";
+        } else {
+            $flexContainerWrap.css("align-items", "stretch");
+            minHeight = 0;
+        }
+        $("style").remove();
+        $('html > head').append($('<style>.flex-item-wrap { min-height: ' + minHeight + '; }</style>'));
+        $("#flex-row-wrap").find(".dropdown-button").text(value + ";");
+        $flexContainerWrap.css("flex-wrap", value);
+        event.stopPropagation();
+    });
+
+
+    $("#add-item").click(function() {
+            var number = ($('div:last-child', $flexContainerWrap).attr("data-number") % 5) + 1;
+            if (isNaN(number)) {
+                number = 1;
+            }
+        $flexContainerWrap.append($("<div class='flex-item-wrap item-" + number + "' data-number=" + number + ">"));
+    });
+
+    $("#remove-item").click(function() {
+        $('div:last-child', $flexContainerWrap).remove();
+    });
 
 });
